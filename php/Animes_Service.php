752748bb40867ba83e22ce2df4c21aa13968e0b8
Tsuki25 <caueppalma@gmail.com>
2021-10-24 14:32:55 -0300

    crud dos animes, erro na sessão id_att

--- php/Animes_Service.php
+++ php/Animes_Service.php
@@ -1,11 +1,11 @@
 <?php
 class Animes_Service{
     
-    private $connection;
+    private $pdo;
     
     public function __construct($dbname, $host, $user, $senha) {//CONSTRUTOR PARA O BANCO DE DADOS
         try{//TRY CATCH PARA TRATAMENTO DE ERROS
-            $this -> connection = new PDO("mysql:dbname=".$dbname.";host=".$host,$user,$senha);//CRIANDO O PDO DO BANCO
+            $this ->pdo= new PDO("mysql:dbname=".$dbname.";host=".$host,$user,$senha);//CRIANDO O PDO DO BANCO
             
         } catch (PDOException $e) {//ERRO DO PDO
             echo "ERRO COM BANCO DE DADOS".$e->getMessage();
@@ -16,42 +16,63 @@ class Animes_Service{
             exit();
         } 
     }
-
-
-    function cadastarAnimes($nomeAnime, $sinopse,$genero,$dataLancamento,$statusLancamento, $animeImagem) {
-        $sql = 'insert into animes (nomeAnime, sinopse, genero, dataLancamento, statusLancamento, animeImagem) 
-        values (:nomeAnime, :sinopse, :genero, :dataLancamento, :statusLancamento, :animeImagem)';
-        $result = $this -> connection -> prepare ($sql);
-        $result -> bindValue(':nomeAnime', $nomeAnime );
-        $result -> bindValue(':sinopse', $sinopse );
-        $result -> bindValue(':genero', $genero  );
-        $result -> bindValue(':dataLancamento', $dataLancamento );
-        $result -> bindValue(':statusLancamento', $statusLancamento );
-        $result -> bindValue(':animeImagem', $animeImagem );
-        $result -> execute();
+    
+    public function buscarAnime($nomeAnime){
+        $cmd = $this->pdo->prepare("SELECT * FROM animes WHERE nomeAnime, = :an");
+        $cmd->bindValue(":an", $nomeAnime);
+        $cmd->execute();
+        $res = $cmd->fetch(PDO::FETCH_ASSOC);
+        return $res;
+    }
+    
+    public function buscarAnimeById($id){
+        $cmd = $this->pdo->prepare("SELECT * FROM animes WHERE idAnime = :id");
+        $cmd->bindValue(":id", $id);
+        $cmd->execute();
+        $res = $cmd->fetch(PDO::FETCH_ASSOC);
+        return $res;
     }
 
 
-    //LER
-    /*$sql = 'select * from animes';
-    $result = $connection -> prepare ($sql);
-    $result -> execute();
-    var_dump($result -> fetchAll(PDO::FETCH_OBJ));*/
+    function cadastarAnimes($nomeAnime, $sinopse,$genero,$dataLancamento,$statusLancamento, $animeImagem) {
+        $cmd = $this->pdo->prepare ('INSERT INTO animes (nomeAnime, sinopse, genero, dataLancamento, statusLancamento, animeImagem) 
+        VALUES (:nomeAnime, :sinopse, :genero, :dataLancamento, :statusLancamento, :animeImagem)');
+        $cmd -> bindValue(':nomeAnime', $nomeAnime );
+        $cmd -> bindValue(':sinopse', $sinopse );
+        $cmd -> bindValue(':genero', $genero  );
+        $cmd -> bindValue(':dataLancamento', $dataLancamento );
+        $cmd -> bindValue(':statusLancamento', $statusLancamento );
+        $cmd -> bindValue(':animeImagem', $animeImagem );
+        $cmd -> execute();
+    }
+    
+    public function atualizacaoDados($nomeAnime, $sinopse,$genero,$dataLancamento,$statusLancamento, $id, $animeImagem){
+        $cmd = $this->pdo->prepare("UPDATE animes SET nomeAnime = :n, sinopse = :s, genero = :g, dataLancamento= :dl, statusLancamento = :sl, animeImagem = :an WHERE idAnime = :id");
+        $cmd -> bindValue(':n', $nomeAnime );
+        $cmd -> bindValue(':s', $sinopse );
+        $cmd -> bindValue(':g', $genero  );
+        $cmd -> bindValue(':dl', $dataLancamento );
+        $cmd -> bindValue(':sl', $statusLancamento );
+        $cmd->bindValue(":id", $id);
+        $cmd -> bindValue(':an', $animeImagem );
+        $cmd->execute();
+    }
     
     public function carregarCards($reg_pag, $pg){
         $inicio = ($pg - 1) * $reg_pag;
         
-        $sql = $this->connection->prepare( "SELECT nomeAnime, sinopse, animeImagem FROM animes LIMIT $inicio,$reg_pag;");//PEGA TODOS OS VIDEOS
-        $sql->execute();
+        $cmd = $this->pdo->prepare( "SELECT idAnime, nomeAnime, sinopse, animeImagem FROM animes LIMIT $inicio,$reg_pag;");//PEGA TODOS OS VIDEOS
+        $cmd->execute();
 
-        if ($sql->rowCount() > 0) {//Enquanto tiverem linhas na tabela
-            foreach ($sql as $res) {
+        if ($cmd->rowCount() > 0) {//Enquanto tiverem linhas na tabela
+            foreach ($cmd as $res) {
+                $id_anime = $res['idAnime'];
                 $nomeAnime = $res['nomeAnime'];
                 $sinopse = $res['sinopse'];
                 $animeImagem = $res['animeImagem'];
                 echo "
                 <div class='card bg-black border-purple pb-4 cursorh-pointer'>
-                <img src='img/animes-banner/$animeImagem.jpg' class='anime-image' alt=''>
+                <img src='img/animes-banner/$animeImagem' class='anime-image' alt=''>
                 <div class='conteudo'>
                     <h2 class='text-center text-light my-2'>$nomeAnime</h2>
                     <div class='card-sinopse'>
@@ -62,8 +83,8 @@ class Animes_Service{
                     </div>
                     <div class='float-right mt-3'>
                         <div class='btn-group'>
-                            <button type='button' class='btn btn-outline-light btn-editar'>Editar</button>
-                            <button type='button' class='btn btn-outline-light btn-excluir'>Excluir</button>
+                            <button type='button' class='btn btn-outline-light btn-editar'><a href='index.php?id_anime=$id_anime' onclick='document.getElementById('adicionarAnime').style.display='none';document.getElementById('formAnime').style.display='block';'>Editar</a></button>
+                            <button type='button' class='btn btn-outline-light btn-excluir' ><a href='index.php?id_excluir=$id_anime'>Excluir</a></button>
                         </div>
                     </div>
                 </div>
@@ -71,7 +92,7 @@ class Animes_Service{
             }
         }
         
-        $total = $this->connection->prepare("SELECT * FROM animes;");
+        $total = $this->pdo->prepare("SELECT * FROM animes;");
         $total->execute();
         $tp = $total->rowCount() / $reg_pag;
         $tp = ceil($tp);
