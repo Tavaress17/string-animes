da2134f95ce351a5b7a7070d8c65c636c3c3eba9
Tavaress17 <luantavares17@gmail.com>
2021-11-02 11:57:58 -0300

    Arrumando alguns problemas de modificação

--- js/sendImage.js
+++
@@ -1,7 +0,0 @@
-const imagem = document.getElementById('imagem');
-
-const fileChosen = document.getElementById('file-chosen');
-
-actualBtn.addEventListener('change', function(){
-    fileChosen.textContent = this.files[0].name
-})
